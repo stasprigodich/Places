1ba323826b2bdc9666227dd3b5de9c91557a9bb8
Stan Prigodich <stan@backbase.com>
2024-08-29 10:13:24 +0200

    Initial commit

---
+++ places/placesApp.swift
@@ -0,0 +1,17 @@
+//
+//  placesApp.swift
+//  places
+//
+//  Created by Stan Prigodich on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct placesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
