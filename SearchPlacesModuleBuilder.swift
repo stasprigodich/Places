baf0a1c6cfd5da11ff7785586b22ca9c0ceb5404
Stanislav Prigodich <stas.prigodich@gmail.com>
2024-08-29 10:47:11 +0200

    add project structure - builder and view

---
+++ SearchPlacesModuleBuilder.swift
@@ -0,0 +1,14 @@
+//
+//  PlacesModuleBuilder.swift
+//  places
+//
+//  Created by Stan Prigodich on 29/08/2024.
+//
+
+import SwiftUI
+
+struct SearchPlacesModuleBuilder {
+    static func build() -> some View {
+        return SearchPlacesView()
+    }
+}
---
+++ places/Modules/SearchPlaces/View/SearchPlacesView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPlacesView.swift
+//  places
+//
+//  Created by Stan Prigodich on 29/08/2024.
+//
+
+import SwiftUI
+
+struct SearchPlacesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPlacesView()
+}
